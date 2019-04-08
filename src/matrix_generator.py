c631bc38c5b1a4899cdfbd8f9967385ee764b59f
Alessandro994 <alebacchiega51@gmail.com>
2019-04-08 18:04:06 +0200

    add script to distinguish earth from water

--- src/matrix_generator.py
+++ src/matrix_generator.py
@@ -1,7 +1,24 @@
 from skimage import io
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
 
-filename = '../data/examples/map5.png'
-image = io.imread(filename)
+image_path = sys.argv[1]
+image = io.imread(image_path)
 
+rows = image.shape[0]
+columns = image.shape[1]
 
+mask = my_array = np.zeros([rows, columns])
 
+for r in range(rows):
+	for c in range(columns):
+		pixel = image[r][c]
+		if (pixel[0] == 38 and pixel[1] == 64 and pixel[2] == 73) or (
+			pixel[0] == 51 and pixel[1] == 83 and pixel[2] == 95):
+			mask[r][c] = 0
+		else:
+			mask[r][c] = 1
+
+plt.imshow(mask)
+plt.show()

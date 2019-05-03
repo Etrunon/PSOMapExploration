950df9c4d90143f729bd2db8800ec4fae7e99741
Luca Bosotti <bosotti@fbk.eu>
2019-05-03 14:52:14 +0200

    Create preprocess.py to call all the other refactor detectors as functions to be called from outside clean unused scripts

--- src/detect_resources.py
+++ src/detect_resources.py
@@ -1,11 +1,3 @@
-from PIL import Image
-from numpy.core.multiarray import ndarray
-from matplotlib.figure import Figure
-from skimage import io
-from skimage.feature import blob_log
-import sys
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 import numpy as np
 
 # This script controls the detection of all different resources: coal, iron, copper and uranium
@@ -28,65 +20,27 @@ resourceColours = {
 }
 
 
-def highlight_resource(res_matrix, image, resName, on_map):
+def __highlight_resource(res_matrix, image, resName, on_map):
 	searching = resourceColours[resName]
 	# Scan the image looking for the color
-	for i in range(0, row):
-		for j in range(0, col):
+	for i in range(0, image.shape[0]):
+		for j in range(0, image.shape[1]):
 			pixel = image[i][j]
 			if pixel[0] == searching[0] and pixel[1] == searching[1] and pixel[2] == searching[2]:
 				res_matrix[i][j] = on_map
 				res_matrix[i][j] = on_map
 				res_matrix[i][j] = on_map
 
+	return res_matrix
+
+
+def detect_resource(img_array):
+	# Find all resources and put them on the matrix
+	result = np.zeros((img_array.shape[0], img_array.shape[1]))
+	for i in range(0, len(resources)):
+		__highlight_resource(result, img_array, resources[i], i+1)
+		print("Finito il preprocessing di " + resources[i])
+
+	print("Finito il preprocessing: ")
+
 	return result
-
-
-# Load the image
-image_name = sys.argv[1]
-img = Image.open("data/examples/" + image_name)
-img_array = np.asarray(img, dtype="int32")
-row = len(img_array)
-col = len(img_array[0])
-
-# Find all resources and put them on the matrix
-result = np.zeros((row, col))
-for i in range(0, len(resources)):
-	highlight_resource(result, img_array, resources[i], i+1)
-	print("Finito il preprocessing di " + resources[i])
-
-print("Finito il preprocessing: ")
-imgPlot = plt.imshow(result)
-plt.show()
-
-# Algoritmo trova blob
-# blobs_log = blob_log(result, min_sigma=0.1, num_sigma=10, threshold=.05)
-# print("Finita la ricerca dei blob")
-#
-# print("X" * 100)
-# for blob in blobs_log:
-# 	y, x, radius = blob
-#
-# 	if radius > 1:
-# 		print("x: " + str(x) + " y: " + str(y) + " radius: " + str(radius))
-
-# colors = ['yellow', 'lime', 'red']
-# titles = ['Laplacian of Gaussian', 'Difference of Gaussian', 'Determinant of Hessian']
-# sequence = zip(blobs_list, colors, titles)
-#
-# fig: Figure
-# axes: ndarray
-# fig, axes = plt.subplots(1, 2, figsize=(9, 3), sharex=True, sharey=True)
-# ax = axes.ravel()
-
-# for idx, (blobs, color, title) in enumerate(sequence):
-# 	ax[idx].set_title(title)
-# 	ax[idx].imshow(result, interpolation='nearest')
-# 	for blob in blobs:
-# 		y, image_path, r = blob
-# 		c = plt.Circle((image_path, y), r, color=color, linewidth=0.1, fill=False)
-# 		ax[idx].add_patch(c)
-# 	ax[idx].set_axis_off()
-#
-# plt.tight_layout()
-# plt.show()
---
+++ src/detect_water.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def detect_water(data):
+	# Create matrix for the result
+	rows = data.shape[0]
+	columns = data.shape[1]
+	result = np.zeros((rows, columns))
+
+	# Scan image looking for the colors
+	for r in range(0, rows):
+		for c in range(0, columns):
+			if (data[r][c][0] == 38 and data[r][c][1] == 64 and data[r][c][2] == 73) or (
+				data[r][c][0] == 51 and data[r][c][1] == 83 and data[r][c][2] == 95):
+				result[r][c] = 1
+
+	return result

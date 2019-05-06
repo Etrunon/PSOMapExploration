28cf87c49ddce8b77ea656f65bfa58abd7eda40b
Luca Bosotti <bosotti@fbk.eu>
2019-05-06 17:33:49 +0200

    add evaluator function that computes the fitness score for a given point    such that the function computes compromises of distance and resource    values rename resource_range to resource_radius to simplify

--- src/Particle.py
+++ src/Particle.py
@@ -4,59 +4,65 @@ from src.Map import Map
 
 
 class Particle:
+    current_position = (None, None)
+    velocity = None
+    local_best = (None, None, None)
+    starting_base = (None, None)
+    resource_radius = None
 
-	current_position = (None, None)
-	velocity = None
-	local_best = (None, None, None)
-	starting_base = (None, None)
-	resource_range = None
+    def __init__(self, current_position, velocity, starting_base, resource_range):
+        self.current_position = np.array([current_position[0], current_position[1]])
+        self.velocity = velocity
+        self.local_best = (0, 0, 0)
+        self.starting_base = np.array([starting_base[0], starting_base[1]])
+        self.resource_radius = resource_range
 
-	def __init__(self, current_position, velocity, starting_base, resource_range ):
-		self.current_position = np.array([current_position[0], current_position[1]])
-		self.velocity = velocity
-		self.local_best = (0, 0, 0)
-		self.starting_base = np.array([starting_base[0], starting_base[1]])
-		self.resource_range = resource_range
+    def count_resources(self, map: Map):
+        """
+        Compute the value of all resources around the square.
+        The computation finds the actual bounding box around the chosen point, making sure not to get outside the
+        limits of the matrix (in the case the chosen point is too close to the border).
+        The idea is to check if each vertex of the bounding box is in a legal position and if not replace it with the border.
+        As a note, v3 is actually not needed.
+        :return: the number of resources
+        """
+        # v1                   v2
+        #      true_v1------true_v2--------
+        #      |
+        #      |      cp
+        #      |
+        # v4   true_v4         v3
 
-	def count_resources(self, map: Map):
-		"""
-		Compute the value of all resources around the square.
-		The computation finds the actual bounding box around the chosen point, making sure not to get outside the
-		limits of the matrix (in the case the chosen point is too close to the border).
-		The idea is to check if each vertex of the bounding box is in a legal position and if not replace it with the border.
-		As a note, v3 is actually not needed.
-		:return: the number of resources
-		"""
+        v1 = (self.current_position[0] - self.resource_radius, self.current_position[1] - self.resource_radius)
+        v2 = (self.current_position[0] + self.resource_radius, self.current_position[1] - self.resource_radius)
+        v3 = (self.current_position[0] + self.resource_radius, self.current_position[1] + self.resource_radius)
+        v4 = (self.current_position[0] - self.resource_radius, self.current_position[1] + self.resource_radius)
 
-		# v1                   v2
-		#      true_v1------true_v2--------
-		#      |
-		#      |      cp
-		#      |
-		# v4   true_v4         v3
+        true_v1 = (max(v1[0], 0), max(v1[1], 0))
+        true_v2 = (min(v2[0], map.map_dim[0]), max(v2[1], 0))
+        true_v3 = (min(v3[0], map.map_dim[0]), min(v3[1], map.map_dim[1]))
+        true_v4 = (max(v4[0], 0), min(v4[1], map.map_dim[1]))
 
-		v1 = (self.current_position[0] - self.resource_range, self.current_position[1] - self.resource_range)
-		v2 = (self.current_position[0] + self.resource_range, self.current_position[1] - self.resource_range)
-		v3 = (self.current_position[0] + self.resource_range, self.current_position[1] + self.resource_range)
-		v4 = (self.current_position[0] - self.resource_range, self.current_position[1] + self.resource_range)
+        # print("true_v1: " + str(true_v1))
+        # print("true_v2: " + str(true_v2))
+        # print("true_v3: " + str(true_v3))
+        # print("true_v4: " + str(true_v4))
 
-		true_v1 = (max(v1[0], 0), max(v1[1], 0))
-		true_v2 = (min(v2[0], map.map_dim[0]), max(v2[1], 0))
-		true_v3 = (min(v3[0], map.map_dim[0]), min(v3[1], map.map_dim[1]))
-		true_v4 = (max(v4[0], 0), min(v4[1], map.map_dim[1]))
+        # Now that we have the box, let's count how many resources are inside
+        res_found = 0
+        print("square range: " + str(true_v1) + ", " + str(true_v2) + ", " + str(true_v3) + ", " + str(true_v4) + ", ")
+        for i in range(true_v1[0], true_v2[0]):
+            for j in range(true_v1[1], true_v4[1]):
+                if map.resource_map[i][j] != 0:
+                    res_found = res_found + 1
 
-		# print("true_v1: " + str(true_v1))
-		# print("true_v2: " + str(true_v2))
-		# print("true_v3: " + str(true_v3))
-		# print("true_v4: " + str(true_v4))
+        # print("res_found " + str(res_found))
+        return res_found
 
-		# Now that we have the box, let's count how many resources are inside
-		res_found = 0
-		for i in range(true_v1[0], true_v2[0]):
-			for j in range(true_v1[1], true_v4[1]):
-				if map.resource_map[i][j] != 0:
-					res_found = res_found + 1
-
-		# print("res_found " + str(res_found))
-		return res_found
-
+    def __str__(self) -> str:
+        return "Particle: \n" + \
+               "\tcurrent_position: " + str(self.current_position) + " \n" + \
+               "\tvelocity: " + str(self.velocity) + " \n" + \
+               "\tlocal_best: " + str(self.local_best) + " \n" + \
+               "\tstarting_base: " + str(self.starting_base) + " \n" + \
+               "\tresource_range : " + str(self.resource_radius) + " \n"
--- src/pso.py
+++ src/pso.py
@@ -1,41 +1,49 @@
+import math
 import sys
 from random import Random
 from typing import Tuple
+from inspyred import benchmarks
 
 import numpy as np
 
 from src.Map import Map
 from src.Particle import Particle
-from src.detect_resources import detect_resource
-from src.detect_water import detect_water
 
 
-def generate(map: Map, starting_base : Tuple[int, int], resource_range, random):
-	"""
-	Take a point on earth and returns its position plus the number of resources within the boundary box.
-	:param map:
-	:param square_range: dimensions of the boundary box
-	:param random: random floating point
-	:return: coordinates of the particle and the number of resources
-	"""
-	while True:
-		random_point = (random.randint(0, map.map_dim[0]), random.randint(0, map.map_dim[1]))
-		# If there is earth under the chosen point break outside, else generate another point
-		if map.water_map[random_point[0]][random_point[1]] == 0:  # 0 means earth in the matrix
-			break
+def generate(map: Map, starting_base: Tuple[int, int], resource_radius, random):
+    """
+    Take a point on earth and returns its position plus the number of resources within the boundary box.
+    :param resource_radius: range in which the particle will be looking for resources
+    :param starting_base: base of operations
+    :param map:
+    :param random: random generator
+    :return: the new particle
+    """
+    while True:
+        random_point = (random.randint(0, map.map_dim[0]), random.randint(0, map.map_dim[1]))
+        # If there is earth under the chosen point break outside, else generate another point
+        if map.water_map[random_point[0]][random_point[1]] == 0:  # 0 means earth in the matrix
+            break
 
-	# print("Random point: " + str(r_point))
-	return Particle(random_point, 0, starting_base, resource_range)
+    # print("Random point: " + str(r_point))
+    return Particle((443, 533), 0, starting_base, resource_radius)
+    # return Particle(random_point, 0, starting_base, resource_range)
 
 
 def evaluator(particle: Particle, map: Map):
-	"""
-	Compute the value of this particle in this location. The formula is: nearby resources - distance
-	:return: 
-	"""""
-	particle.count_resources(map)
-	distance = np.linalg.norm(particle.current_position - particle.starting_base)
-	print("Distance: " + str(distance))
+    """
+    Compute the value of this particle in this location. The formula is: nearby resources - distance
+    :return: the score of given particle 
+    """""
+    res_count = particle.count_resources(map)
+    distance = np.linalg.norm(particle.current_position - particle.starting_base)
+    print("res_count: " + str(res_count))
+    print("Distance: " + str(distance))
+
+    square_area = (particle.resource_radius*2)**2
+    normalization_factor = math.atan(square_area / map.map_dim[0])
+    print("math.tan(normalization_factor): " + str(math.tan(normalization_factor)))
+    return distance * math.tan(normalization_factor) - res_count
 
 
 image_name = sys.argv[1]
@@ -45,6 +53,9 @@ map = Map(image_name)
 rand: Random = Random()
 rand.seed(1)
 
-x = generate(map, resource_range=30, random=rand, starting_base=(0, 0)) # ToDo mettere starting base casuale?
-evaluator(x, map)
+x = generate(map, resource_radius=25, random=rand, starting_base=(0, 0))  # ToDo mettere starting base casuale?
+score = evaluator(x, map)
 print("x: " + str(x))
+print("score x: " + str(score))
+
+print(str())

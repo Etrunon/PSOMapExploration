7bc5909f04c207b778b3a4842970c82ea25073af
Carlo Mion <mion00@gmail.com>
2019-07-02 17:00:23 +0200

    Removed unused run configurations Refactor fitness landscape Modify cache name

--- src/main_landscape.py
+++ src/main_landscape.py
@@ -3,27 +3,16 @@ import sys
 from random import Random
 
 import coloredlogs
-import inspyred
-import matplotlib.pyplot
-import numpy as np
-from PIL import Image
-from matplotlib import pyplot
-from matplotlib.axes import Axes
-from matplotlib.figure import Figure
-from matplotlib.patches import Circle
+import matplotlib
+import matplotlib.pyplot as plt
 
 from src.algorithms.algo1 import Algo1
-from src.configuration import RESOURCE_RANGE, CITY_POSITION, POPULATION_SIZE, COGNITIVE_RATE, INERTIA_RATE, \
-    SOCIAL_RATE
-from src.custom_pso import evaluate_particle, custom_terminator, custom_variator, \
-    custom_observer, CustomPSO
 from src.data_structures.Map import Map
-from src.data_structures.Particle import Particle
 
 logger = logging.getLogger(__name__)
 
 # Set the theme used by matplotlib
-matplotlib.pyplot.style.use("seaborn-bright")
+plt.style.use("seaborn-bright")
 # Set the backend used by matplotlib
 matplotlib.use("Qt5Agg")
 
@@ -39,4 +28,8 @@ if __name__ == "__main__":
     world_map = Map(image_name)
     algorithm = Algo1(world_map)
 
-    algorithm.plot_fitness_landscape()
+    logger.info("Calculating fitness")
+    landscape = algorithm.get_fitness_landscape()
+
+    plt.matshow(landscape)
+    plt.show()
